297a5711baafe8a65cfae8a9df4aeee92e6537e1
Rafael Lorenz <lrz.rafael@gmail.com>
2022-08-26 17:20:07 -0300

    Criadas as variáveis de ambiente

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,16 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   projectId: "ixpgsw",
   video : false,
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureWriter(on, config);
+      //const version.env.version || 'nonprod'
+      //config.env = require(`.cypress/config/${version}.json`);
+      //config.baseUrl = config.env.baseUrl;
+      return config;
     },
     baseUrl: 'https://conexaoqa.herokuapp.com'
   },
